ab7ff12c2925c59607252c6c54a6a0da73f33e94
Thales Gonçalves de Souza <78889415+thalesvw@users.noreply.github.com>
2022-12-19 22:07:00 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+import { IMC, notANumber } from './utils.js'
 
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
@@ -8,12 +10,19 @@ form.onsubmit = event => {
   event.preventDefault()
   const weight = inputWeight.value
   const height = inputHeight.value
+  
+  const showAlertError = notANumber(weight) || notANumber(height)
+  if(showAlertError) {
+    AlertError.open()
+    return;
+  }
+  AlertError.close()
+  
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
   Modal.message.innerText = message
   Modal.open()
 }
 
-function IMC(weight, height) {
-  return (weight / ((height / 100) ** 2)).toFixed(2)
-}
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
